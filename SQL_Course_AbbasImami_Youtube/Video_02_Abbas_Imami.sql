f423ec1b40fae4e6fe7d441c32a77b1caa200dee
arsal3646 <arsal3646@gmail.com>
2025-12-19 13:25:33 +0400

    Trying to setup VS Code and GITHUB

--- SQL_Course_AbbasImami_Youtube/Video_02_Abbas_Imami.sql
+++ SQL_Course_AbbasImami_Youtube/Video_02_Abbas_Imami.sql
@@ -1,6 +1,6 @@
-SELECT TOP(9)* -- What you want to see
+SELECT TOP(9)* -- What you want to see, i.e. the data you want to retrieve
 
-FROM FactInternetSales;  -- From which table you want to see it
+FROM FactInternetSales;  -- From which table you want to see it. This is like a reference to the table from which you want to extract data.
 
 -- There are SIX main clauses in a SQL query:
 -- 1. SELECT
@@ -25,11 +25,16 @@ SalesOrderNumber       -- Never put comma after the last column
 FROM FactInternetSales;
 
 
-SELECT TOP (11)       -- Don't put star because it will select all columns, while now we only want few columns
+SELECT       -- Don't put star because it will select all columns, while now we only want few columns
+    -- SalesTerritoryKey AS CanadaSales,
     OrderDate,
     SalesOrderNumber,   
     SalesAmount,
     TaxAmt,
+    DueDate,
+    CurrencyKey AS 'New Columnn Name Currency', -- This is how you rename a column in SQL. And, this renaming only works in the output, not in the actual table.
     OrderQuantity     -- Don't put comma after the last column otherwise it will give error    
 
-FROM FactInternetSales;
+FROM FactInternetSales
+
+WHERE SalesTerritoryKey = 6;  -- This is how you filter data in SQL. Only the rows that satisfy this condition will be shown in the output.
