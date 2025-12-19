6fdffb8916b5876577d9af7e2c80cf4d86e4273b
arsal3646 <arsal3646@gmail.com>
2025-12-19 22:23:08 +0400

    Updated

--- SQL_Course_AbbasImami_Youtube/Video_03_Abbas_Imami.sql
+++ SQL_Course_AbbasImami_Youtube/Video_03_Abbas_Imami.sql
@@ -1,11 +1,29 @@
-SELECT TOP (100)
+SELECT 
 
     SalesOrderNumber AS InvoiceNumber,
     OrderDate,
-    SUM(SalesAmount),
-    SUM(TaxAmt),
-    SUM(OrderQuantity)
+    SUM(SalesAmount) AS TotalSales,
+    SUM(TaxAmt) AS TaxAmt, 
+    SUM(OrderQuantity) AS QuantitySold 
+
 
 FROM FactInternetSales
 
+WHERE SalesTerritoryKey = 6
+
 GROUP BY SalesOrderNumber, OrderDate
+
+-- WHERE and HAVING clauses are both for filtering data.
+-- WHERE is used to filter rows before aggregation.
+-- HAVING is used to filter groups after aggregation.
+
+HAVING SUM(SalesAmount) >= 1000
+
+ORDER BY -- This clause is used to sort the result set.
+
+SUM(SalesAmount) ASC;
+
+-- ASC = Ascending Order
+-- DESC = Descending Order
+-- Alias can be used in ORDER BY clause but not in WHERE or HAVING clauses.
+-- However, in MYSQL and POSTGRESQL, you can use alias in HAVING clause.
