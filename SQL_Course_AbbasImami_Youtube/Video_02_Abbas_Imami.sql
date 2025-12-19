c895c8aa8493133c4cfceb761ddfd407a2187e63
arsal3646 <arsal3646@gmail.com>
2025-12-19 20:30:17 +0400

    Updated

--- SQL_Course_AbbasImami_Youtube/Video_02_Abbas_Imami.sql
+++ SQL_Course_AbbasImami_Youtube/Video_02_Abbas_Imami.sql
@@ -1,6 +1,6 @@
-SELECT TOP(9)* -- What you want to see, i.e. the data you want to retrieve
+-- SELECT TOP(9)* -- What you want to see, i.e. the data you want to retrieve
 
-FROM FactInternetSales;  -- From which table you want to see it. This is like a reference to the table from which you want to extract data.
+-- FROM FactInternetSales;  -- From which table you want to see it. This is like a reference to the table from which you want to extract data.
 
 -- There are SIX main clauses in a SQL query:
 -- 1. SELECT
@@ -10,31 +10,43 @@ FROM FactInternetSales;  -- From which table you want to see it. This is like a 
 -- 5. HAVING
 -- 6. ORDER BY
 
-SELECT TOP(3)*
+-- SELECT TOP(3)*
 
-FROM FactFinance;
+-- FROM FactFinance;
 
-SELECT TOP(3)*
+-- SELECT TOP(3)*
 
-FROM FactCallCenter;
+-- FROM FactCallCenter;
 
-SELECT TOP (100)       -- Don't put star because it will select all columns, while now we only want few columns
-OrderDate,
-SalesOrderNumber       -- Never put comma after the last column
+-- SELECT TOP (100)       -- Don't put star because it will select all columns, while now we only want few columns
+-- OrderDate,
+-- SalesOrderNumber       -- Never put comma after the last column
 
-FROM FactInternetSales;
+-- FROM FactInternetSales;
 
 
 SELECT       -- Don't put star because it will select all columns, while now we only want few columns
     -- SalesTerritoryKey AS CanadaSales,
-    OrderDate,
+
+    OrderDate, -- Aggregation cannot be done on DATE fields as these are non-numerical fields
     SalesOrderNumber,   
-    SalesAmount,
-    TaxAmt,
-    DueDate,
-    CurrencyKey AS 'New Columnn Name Currency', -- This is how you rename a column in SQL. And, this renaming only works in the output, not in the actual table.
-    OrderQuantity     -- Don't put comma after the last column otherwise it will give error    
+    SUM(SalesAmount) AS 'Total Sales Amount',   -- SUM is an AGGREGATION function in SQL. It adds up all the values in the specified column.
+    SUM(TaxAmt) AS 'Total Tax Amount',
+    -- DueDate,
+    -- CurrencyKey AS 'New Columnn Name Currency', -- This is how you rename a column in SQL. And, this renaming only works in the output, not in the actual table.
+    sum(OrderQuantity) AS 'Total Quantity Sold'     -- Don't put comma after the last column otherwise it will give error    
 
 FROM FactInternetSales
+WHERE SalesTerritoryKey = 6  -- This is how you filter data in SQL. Only the rows that satisfy this condition will be shown in the output.
 
-WHERE SalesTerritoryKey = 6;  -- This is how you filter data in SQL. Only the rows that satisfy this condition will be shown in the output.
+
+-- AGGREGATION - We need tax, quantity sold, and sales amount for each invoice (similar to SUMIF in Excel)
+-- AGGREGATION is always done on NUMERICAL fields
+
+-- WHERE SalesOrderNumber = 'SO51182'
+
+-- In GROUP BY clause, 
+-- (1) we need to mention the columns on basis of which we want to aggregate the data, and 
+-- (2) mentioned how we want to display the non-aggregated columns. 
+
+GROUP BY SalesOrderNumber, OrderDate
