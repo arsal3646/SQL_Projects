df5458b337d008ec9a5d00eef900991a5cc777da
arsal3646 <arsal3646@gmail.com>
2026-01-03 19:13:43 +0400

    IN, LIKE, BETWEEN, PATTERN-MATCHING

--- SQL_Course_AbbasImami_Youtube/Video_09_LIKE_IIF_Abbas_Imami.sql
+++ SQL_Course_AbbasImami_Youtube/Video_09_LIKE_IIF_Abbas_Imami.sql
@@ -1,8 +1,39 @@
-SELECT TOP (1000) *
+SELECT 
+    EnglishProductName,
+    Class,
+    [Size],
+    Color,
+    [Status],
+    SafetyStockLevel
 
 
-FROM FactInternetSales
-WHERE CurrencyKey = 19;
+FROM DimProduct
+
+-- WHERE Color = 'Blue' OR Color = 'Black' OR Color = 'White' OR Color = 'Yellow' -- This can be replaced by IN operator
+-- WHERE Color IN ('Blue', 'Black', 'White', 'Yellow')
+
+-- WHERE SafetyStockLevel IN(500,800,1000)
+-- WHERE SafetyStockLevel NOT IN(500,800,1000)
+-- WHERE SafetyStockLevel >=500 AND SafetyStockLevel <=1000
+
+-- IN always works with OR condition while NOT IN works with AND condition
+-- BETWEEN operator is used for range checking. It works with AND condition
+
+WHERE SafetyStockLevel BETWEEN 500 AND 1000;
 
 
--- Video watched until minute 
+-- LIKE operator is used for pattern matching
+
+SELECT 
+    FirstName,
+    EmailAddress
+    
+
+FROM DimCustomer
+
+-- WHERE FirstName LIKE '_r%'  -- Names with second letter 'r'
+-- WHERE FirstName LIKE 'A%'  -- Names starting with 'A'
+
+WHERE FirstName LIKE 'r__' -- Names with 3 letters starting with 'r'. These are two underscores after r.
+
+-- Video watched until minute 23
