4f4c14801952cd8c67b24cd10c39c29a336c7f1e
arsal3646 <arsal3646@gmail.com>
2026-09-13 09:26:27 +0400

    Session 30 min 21

--- SQL_CampusX_DSMP_2023/Session_30_Database_Fundamentals.sql
+++ SQL_CampusX_DSMP_2023/Session_30_Database_Fundamentals.sql
@@ -1,13 +1,44 @@
 /* We will study this whole session from the perspective of a data analyst
 
-! This session will cover the basics of databases, including tables, queries, and relationships.
-! By the end of this session, you should have a basic understanding of how to work with databases as a data analyst.
-! We will also explore some common SQL commands and how to use them effectively.
-! By the end of this session, you should be able to write basic SQL queries to retrieve and manipulate data from a database.
-! You will also gain an understanding of how to design and structure a database to efficiently store and manage data.
-! Finally, we will discuss best practices for maintaining and optimizing a database to ensure its performance and reliability.
-! a comment in SQL starts with -- for single-line comments or /* */ for multi-line comments.
-! For example:
-! -- This is a single-line comment
-! /* This is a
-!    multi-line comment */
+This session will cover the basics of databases, including 
+    1. tables
+    2. queries
+    3. relationships.
+
+By the end of this session, you should have a basic understanding of how to work with databases as a data analyst.
+
+We will also explore some common SQL commands and how to use them effectively.
+
+By the end of this session, you should be able to write basic SQL queries to 
+    - retrieve data from a database 
+    - manipulate data
+
+You will also gain an understanding of how to design and structure a database to efficiently store and manage data.
+
+Finally, we will discuss best practices for 
+    - maintaining
+    - optimizing a database to ensure its performance and reliability.
+
+A comment in SQL starts with -- for single-line comments or /* */ for multi-line comments.
+For example:
+-- This is a single-line comment
+*/
+
+/* This is a multi-line comment */  
+
+/* What is Database? 
+
+- A database is an organized collection of data, generally stored and accessed electronically from a computer system.
+- Databases are used to efficiently 
+        -   store
+        -   retrieve, and 
+        -   manage data for various applications.
+
+- Databases can be: 
+        -   relational, where data is organized into tables with relationships, or 
+        - non-relational, where data is stored in a more flexible format.
+
+- Databases play a crucial role in modern applications, 
+- as it is enabling efficient data management and supporting various data-driven decision-making processes.
+
+*/
