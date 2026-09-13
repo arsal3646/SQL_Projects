4070db419008811abf2989daa6138dc3786e69f6
arsal3646 <arsal3646@gmail.com>
2026-09-13 09:15:18 +0400

    .

---
+++ SQL_CampusX_DSMP_2023/Session_30_Database_Fundamentals.sql
@@ -0,0 +1,13 @@
+/* We will study this whole session from the perspective of a data analyst
+
+! This session will cover the basics of databases, including tables, queries, and relationships.
+! By the end of this session, you should have a basic understanding of how to work with databases as a data analyst.
+! We will also explore some common SQL commands and how to use them effectively.
+! By the end of this session, you should be able to write basic SQL queries to retrieve and manipulate data from a database.
+! You will also gain an understanding of how to design and structure a database to efficiently store and manage data.
+! Finally, we will discuss best practices for maintaining and optimizing a database to ensure its performance and reliability.
+! a comment in SQL starts with -- for single-line comments or /* */ for multi-line comments.
+! For example:
+! -- This is a single-line comment
+! /* This is a
+!    multi-line comment */
