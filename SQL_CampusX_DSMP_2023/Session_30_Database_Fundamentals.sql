fc10f346741d3b41864a55ba3167a9b24c54cff6
arsal3646 <arsal3646@gmail.com>
2026-09-13 12:11:26 +0400

    Types of databases

--- SQL_CampusX_DSMP_2023/Session_30_Database_Fundamentals.sql
+++ SQL_CampusX_DSMP_2023/Session_30_Database_Fundamentals.sql
@@ -60,3 +60,56 @@ CRUD stands for Create, Read, Update, and Delete, which are the four basic opera
     - Delete: Removing records from a database table.
 
 */
+
+/*
+Properties of an ideal database include:
+
+    - Data integrity: 
+            1. Ensuring accuracy, e.g. no negative values for age or salary.
+            2. Ensuring consistency of data, e.g. no conflicting information across different tables.
+
+    - Availability: Ensuring that the database is accessible when needed. 
+            e.g. minimal downtime and quick recovery from failures.
+
+    - Security: Protecting data from unauthorized access. 
+            e.g. using encryption and access controls.
+    
+    - Independent of Application: The database should function independently of the application using it. 
+            e.g. changes in the application should not affect the database structure.
+
+    - Concurrency: The database should support multiple users accessing and modifying data simultaneously without conflicts. 
+            e.g. using locking mechanisms and transaction management.
+*/
+
+/*  Types of Databases:
+
+    - Relational Databases: 
+            - Organize data into tables with predefined relationships. 
+            - Examples include MySQL, PostgreSQL, and Oracle.
+            - Suitable for structured data with clear relationships.
+            - Suitable for scenarios where the schema may evolve over time and flexibility is required.
+            - Commonly used in web and mobile applications where rapid development and scalability are important.
+            - Often used in scenarios where data integrity and complex queries are important.
+
+    - NoSQL Databases: 
+            - Store data in a more flexible format, such as key-value pairs, documents, or graphs. 
+            - Examples include MongoDB, Cassandra, and Redis.
+    
+    
+    - Column Databases: 
+            - Store data in columns rather than rows, which can improve performance for certain types of queries. 
+            - Examples include Apache Cassandra and HBase.
+    
+    
+    - Graph Databases: 
+            - Store data in graph structures with nodes, edges, and properties, which is useful for analyzing relationships. 
+            - Examples include Neo4j and Amazon Neptune.
+   
+   
+    - Key-value Databases: 
+            - Store data as key-value pairs, which allows for fast retrieval based on the key. 
+            - Examples include Redis and Amazon DynamoDB.
+
+*/
+
+-- Watched the video until min 29
