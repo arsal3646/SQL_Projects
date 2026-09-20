a0e2a587b4b1955fe962837b0711183d27cbcb42
arsal3646 <arsal3646@gmail.com>
2026-09-20 20:53:00 +0400

    minute 12

--- SQL_CampusX_DSMP_2023/Session_31_SQL_DDA_Commands.sql
+++ SQL_CampusX_DSMP_2023/Session_31_SQL_DDA_Commands.sql
@@ -1,4 +1,52 @@
+/*  ========================
+    What is SQL? 
+    SQL stands for Structured Query Language. 
+    It is used to communicate with and manipulate databases. 
+    SQL allows you to create, read, update, and delete data in a database.
 
+    ========================
+
+    Types of SQL Commands:
+
+    1. DDL (Data Definition Language)
+       
+       - Used to define and manage database structures such as tables, indexes, and schemas.
+       
+       - Commands include 
+        1. CREATE  - create a new database object such as a table or index
+        2. ALTER   - modify an existing database object
+        3. DROP    - delete an existing database object
+        4. TRUNCATE - remove all records from a table, but keep the table structure
+    
+    2. DML (Data Manipulation Language)
+       
+       - Used to retrieve, insert, update, and delete data in a database.
+       
+       - Commands include 
+        1. SELECT - retrieve data from the database
+        2. INSERT - add new data to the database
+        3. UPDATE - modify existing data in the database
+        4. DELETE - remove data from the database
+    
+    3. DCL (Data Control Language)
+       
+       - Used to control access to data in the database, such as granting and revoking permissions.
+       
+       - Commands include 
+        1. GRANT - give specific privileges to a user
+        2. REVOKE - remove specific privileges from a user
+    
+    4. TCL (Transaction Control Language)
+      
+       - Used to manage transactions in the database, such as committing and rolling back changes.
+      
+       - Commands include 
+        1. COMMIT - save all changes made in the current transaction
+        2. ROLLBACK - undo all changes made in the current transaction
+        3. SAVEPOINT - set a point within a transaction to which you can later roll back
+        4. SET TRANSACTION - configure the properties of the current transaction
+
+*/
 
 
 -- Watched video 31 until 0 hr 2 min (total video length is 2 hr 04 min)
