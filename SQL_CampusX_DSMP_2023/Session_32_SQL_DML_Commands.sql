8f5b510f9d40b4109d624ec5bead4203ac769182
arsal3646 <arsal3646@gmail.com>
2026-09-26 10:13:48 +0400

    Started Video 32

--- SQL_CampusX_DSMP_2023/Session_32_SQL_DML_Commands.sql
+++ SQL_CampusX_DSMP_2023/Session_32_SQL_DML_Commands.sql
@@ -1,1 +1,14 @@
--- Watched video 32 until 0 hr 0 min (total video length is  hr  min)
+/*
+
+-- DML is the most interesting part of SQL as it allows us to manipulate and interact with the data stored in the database.
+-- In this session we will cover SQL DML (Data Manipulation Language) commands such as INSERT, UPDATE, DELETE, and SELECT.
+
+
+*/
+
+-- sudo /Applications/XAMPP/xamppfiles/xampp start
+
+-- verify using sudo /Applications/XAMPP/xamppfiles/xampp status
+-- http://localhost/phpmyadmin
+
+-- Watched video 32 until 0 hr 0 min (total video length is  2 hr 04 min)
